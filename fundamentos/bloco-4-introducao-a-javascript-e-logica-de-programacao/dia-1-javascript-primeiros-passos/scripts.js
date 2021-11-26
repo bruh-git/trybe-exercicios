178ecb9032ca143081deb1ce2395023a345d36a2
Bruna <brunac.cardososilva@gmail.com>
2021-11-26 19:17:44 -0300

    exercicios dia 4.1

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
@@ -0,0 +1,33 @@
+
+//const myName="Bruna";{
+   // console.log(myName);
+
+
+//const brithCity = "Contagem";{
+   // console.log(brithCity);
+
+
+//let brithYear ="1998";
+//brithYear = "2030";
+// console.log(brithYear);
+
+// let patientId = 50;
+// patientId="50";
+// let isEnrolled = true;
+// const patientInfo = {
+//   firstName: 'Ana',
+//   lastName: 'Santos',
+// };
+// const patientEmail = 'ana@email.com';
+
+// console.log(typeof patientId)
+
+// const base = 5;
+// const height =8;
+// const area = (base*height)
+// console.log(area);
+
+// const perimeter=(base+height)*2;
+// console.log(perimeter);
+
+
