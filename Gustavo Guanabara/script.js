ef9295e031ee0f44fd6badbc027ee021db6215fd
Bruna <brunac.cardososilva@gmail.com>
2022-03-03 23:02:03 -0300

    corrigindo

---
+++ Gustavo Guanabara/script.js
@@ -0,0 +1,25 @@
+// let resultado = prompt ('Qual o seu nome ?');
+// let nome = resultado;
+
+// alert (`Seja bem vindo(a) ${nome}!`);
+
+// fiz com minha logica 
+
+
+// let nome = prompt('Qual é seu nome?');
+// alert(`Seja bem vindo(a) ${nome}`);
+// maneira que o Guanabara fez!
+
+// let n1 = Number(prompt('Digite um numero: '));
+// let n2 = Number(prompt('Digite outro numero: '));
+// usa o number para converter de string para number 
+
+// let soma = n1 + n2;
+
+// alert(`A soma de ${n1} e ${n2} é igual a ${soma}`);
+
+// let nome = prompt('Qual o seu nome?');
+
+// alert(`Olá ${nome} seu nome tem ${nome.length} letras 
+// em maiusculo fica assim: ${nome.toLocaleUpperCase()} 
+// e em minusculo fica dessa forma: ${nome.toLocaleLowerCase()}`);
